36d333b525a7e489efafaadc1b9cb854483a34f1
EC2 Default User <ec2-user@ip-172-31-41-206.ap-northeast-1.compute.internal>
2023-07-30 13:34:50 +0000

    [投稿]riders

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,35 @@
+class Public::UsersController < ApplicationController
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def withdrawal
+  end
+
+  def deleteprocess
+    
+    current_user.update(is_active: false)
+    reset_session
+    redirect_to root_path
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to my_page_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :nickname, :profile_image, :introduction, :telephone_number, :email,)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,11 @@ Rails.application.routes.draw do
     root to: "homes#top"
     get '/about' => 'homes#about', as: 'about'
     get '/users/my_page' => 'users#show', as: 'my_page'
-    get '/users/edit' => 'users#edit', as: 'edit_customer'
+    get '/users/information/edit' => 'users#edit', as: 'edit_customer'
+    get '/users/withdrawal' => 'users#withdrawal', as: 'withdrawal'
+    patch '/users/information' => 'users#update', as: 'customer'
+    patch '/users/deleteprocess' => 'users#deleteprocess', as: 'deleteprocess'
+    resources :posts
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
