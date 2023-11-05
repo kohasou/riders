46cc4f344661208d1e96759a1b138a281a56b543
EC2 Default User <ec2-user@ip-172-31-41-206.ap-northeast-1.compute.internal>
2023-11-05 13:15:22 +0000

    通知機能の修正

--- db/migrate/20231008134623_create_notifications.rb
+++ db/migrate/20231008134623_create_notifications.rb
@@ -3,8 +3,8 @@ class CreateNotifications < ActiveRecord::Migration[6.1]
     create_table :notifications do |t|
       t.integer :visitor_id, null: false
       t.integer :visited_id, null: false
+      t.integer :post_id
       t.integer :comment_id
-      t.integer :reply_id
       t.string :action, default: '', null: false
       t.boolean :checked, default: false, null: false
 
@@ -14,7 +14,7 @@ class CreateNotifications < ActiveRecord::Migration[6.1]
     add_index :notifications, :visitor_id
     add_index :notifications, :visited_id
     add_index :notifications, :post_id
-    add_index :notifications, :reply_id
+    add_index :notifications, :comment_id
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -76,13 +76,13 @@ ActiveRecord::Schema.define(version: 2023_10_08_134623) do
     t.integer "visitor_id", null: false
     t.integer "visited_id", null: false
     t.integer "post_id"
-    t.integer "reply_id"
+    t.integer "comment_id"
     t.string "action", default: "", null: false
     t.boolean "checked", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["comment_id"], name: "index_notifications_on_comment_id"
     t.index ["post_id"], name: "index_notifications_on_post_id"
-    t.index ["reply_id"], name: "index_notifications_on_reply_id"
     t.index ["visited_id"], name: "index_notifications_on_visited_id"
     t.index ["visitor_id"], name: "index_notifications_on_visitor_id"
   end
