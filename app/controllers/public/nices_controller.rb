7f3ed6eb95399729d9b18c558132a6256154936c
EC2 Default User <ec2-user@ip-172-31-41-206.ap-northeast-1.compute.internal>
2023-11-23 12:20:37 +0000

    コード修正

--- app/controllers/public/nices_controller.rb
+++ app/controllers/public/nices_controller.rb
@@ -2,7 +2,6 @@ class Public::NicesController < ApplicationController
   def create
     #いいね作成
     post = Post.find(params[:post_id])
-    post.create_notification_nice(current_user)
     #いいねを保存
     nice = Nice.new(post_id: post.id, user_id: current_user.id)
     nice.save
